f209329d879c573e43fd3ea900d65ea335ecabbe
armaan535kh <kh.armaan535@gmail.com>
2026-06-12 15:42:05 +0530

    Initial Commit

---
+++ PrimeNum/PrimeNumApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrimeNumApp.swift
+//  PrimeNum
+//
+//  Created by Armaan Khan  on 12/06/26.
+//
+
+import SwiftUI
+
+@main
+struct PrimeNumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PrimeNumTests/PrimeNumTests.swift
@@ -0,0 +1,17 @@
+//
+//  PrimeNumTests.swift
+//  PrimeNumTests
+//
+//  Created by Armaan Khan  on 12/06/26.
+//
+
+import Testing
+@testable import PrimeNum
+
+struct PrimeNumTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
